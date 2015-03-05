c74ffa480ae70c4fd218e32cc185d25e365b3426
Chuck Jazdzewski <chuckj@google.com>
2015-03-05 09:15:48 -0800

    Updating demo

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,31 @@
+import cgi
 from google.appengine.api import users
-
 import webapp2
 
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <p>Hello, World!</p>
+    <p>What's on your mind?</p>
+    <form action="/repeat" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Record your thoughts!"></div>
+    </form>
+  </body>
+</html>
+"""
+
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
-        # Checks for active Google account session
-        user = users.get_current_user()
+        self.response.write(MAIN_PAGE_HTML)
 
-        if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write('Hello, ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+class Repeater(webapp2.RequestHandler):
+    def post(self):
+        self.response.write('<html><body>You wrote:<pre>')
+        self.response.write(cgi.escape(self.request.get('content')))
+        self.response.write('</pre></body></html>')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/repeat', Guestbook),
 ], debug=True)
